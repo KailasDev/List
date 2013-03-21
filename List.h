1fe357f68ea0ada2b016f939ada41baf27ace277
KailasDev <kailas3476@gmail.com>
2013-03-21 21:22:03 +0100

    add define

--- List.h
+++ List.h
@@ -10,9 +10,15 @@ typedef struct item item;
 typedef item *List;
 
 List List_add(List, int);
+#define List_add(list, value) list = List_add(list, value)
+
 List List_addMany(List, int, ...);
+#define List_addMany(list, number, ...) list = List_addMany(list, number, __VA_ARGS__)
+
 int List_get(List, int);
 void List_display(List);
 int List_size(List);
 int List_head(List);
+
 List List_queue(List);
+#define List_queue(list) list = List_queue(list)
